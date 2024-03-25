d40f2b820d031ebc1a56609ec8135f0a52e4fa73
magysil <magallysilvab@gmail.com>
2024-03-24 21:43:45 -0300

    Se agregó el archivo readme

---
+++ readme.mk
@@ -0,0 +1,26 @@
+# Proyecto: Animales en Investigación
+
+Este proyecto consiste en una aplicación web para registrar información sobre animales en investigación. Permite al usuario ingresar datos sobre diferentes animales y visualizarlos en una interfaz interactiva.
+
+## Tabla de Contenidos
+
+- [Descripción](#descripción)
+- [Archivos](#archivos)
+- [Uso](#uso)
+
+## Descripción
+
+Este proyecto consta de archivos HTML, CSS y JavaScript para crear una aplicación web que permite registrar información sobre animales en investigación. Los archivos HTML (`index.html`) proporcionan la estructura de la página web, mientras que los archivos CSS (`assets/css/estilos.css`) se utilizan para personalizar la apariencia de la página. El archivo JavaScript (`assets/js/main.js`) contiene la lógica de la aplicación, incluyendo la interacción con el usuario y el manejo de datos.
+
+## Archivos
+
+- `index.html`: Archivo HTML principal que contiene la estructura de la página web.
+- `assets/css/estilos.css`: Archivo CSS que contiene los estilos para personalizar la apariencia de la página.
+- `assets/js/main.js`: Archivo JavaScript que contiene la lógica de la aplicación, incluyendo la interacción con el usuario y el manejo de datos.
+
+## Uso
+- Completa los campos del formulario para registrar un nuevo animal.
+- Haz clic en el botón "Agregar" para guardar la información del animal.
+- Los animales registrados se mostrarán en la sección correspondiente de la página.
+- Haz clic en la imagen de un animal para ver más detalles en un modal.
+
